0facf1c7caa74b3b765b4a08855347a113d05780
Matias Heino <matias.heino@hotmail.com>
2025-09-21 18:09:06 +0300

    Add new item form

--- schema.sql
+++ schema.sql
@@ -3,3 +3,13 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE items (
+    id INTEGER PRIMARY KEY,
+    species TEXT,
+    amount INTEGER,
+    place TEXT,
+    city TEXT,
+    description TEXT,
+    user_id INTEGER REFERENCES users(id)
+);
