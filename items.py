58add2610332d5c4279cac04db49e7808110b9cf
Matias Heino <matias.heino@hotmail.com>
2025-09-21 22:47:39 +0300

    Show item list/info

--- items.py
+++ items.py
@@ -3,3 +3,22 @@ import db
 def add_item(species, amount, place, city, description, user_id):
     sql = "INSERT INTO items (species, amount, place, city, description, user_id) VALUES (?, ?, ?, ?, ?, ?)"
     db.execute(sql, [species, amount, place, city, description, user_id])
+
+def get_items():
+    sql = "SELECT id, species, city FROM items ORDER BY id DESC"
+    return db.query(sql)
+
+def get_item(item_id):
+    sql = """SELECT items.species,
+                    items.amount,
+                    items.place,
+                    items.city,
+                    items.description,
+                    users.username
+             FROM items
+             JOIN users ON items.user_id = users.id
+             WHERE items.id = ?"""
+    result = db.query(sql, [item_id])
+    if result:
+        return result[0]
+    return None
